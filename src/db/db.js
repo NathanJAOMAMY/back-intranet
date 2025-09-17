3f9324a28d0a2a47118cb4eaaddf6f936d254a43
N-mika <nadrasanamichael@gmail.com>
2025-09-17 15:49:33 +0300

    alefa

--- src/db/db.js
+++ src/db/db.js
@@ -3,7 +3,7 @@ const initDb = require('./initDb.js');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.ATLAS_URI, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
