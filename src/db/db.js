2dd997eb32ffd088accef54f00d7d93adcf0c50d
N-mika <nadrasanamichael@gmail.com>
2025-09-18 08:29:24 +0300

    push ely maloha

--- src/db/db.js
+++ src/db/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const initDb = require('./initDb.js');
 
 const connectDB = async () => {
+  console.log(process.env.MONGO_URI)
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
